c04b7fd99f4dd2490d6d78147e22b45653e5966f
Matthew Parker <mrparker909@gmail.com>
2019-12-03 11:20:50 -0800

    Create scrapeWebLinks.R

---
+++ Rscripts/scrapeWebLinks.R
@@ -0,0 +1,37 @@
+library(rvest)
+library(stringr)
+
+# input trait number on command line
+args = commandArgs(trailingOnly = T)
+TRAIT_NUM = args[1]
+
+# retrieve list of links on trait page
+page  <- read_html(paste0("http://geneatlas.roslin.ed.ac.uk/downloads/?traits=",TRAIT_NUM))
+links <- page %>% html_nodes("a") %>% html_attr("href")
+
+# imputed links:
+imp_links     = links[stringr::str_which(links,pattern = "/imputed\\.")]
+chr_imp_links = imp_links[1:22]
+
+# imputed snp links:
+imp_snp_links = links[stringr::str_which(links, pattern = "snps\\.imputed")]
+snp_imp_links = imp_snp_links[1:22]
+
+# download files:
+i = 1
+for(li in chr_imp_links) {
+  download.file(url = li, destfile = paste0("./atlasFiles/imputed.chr",i,".csv.gz"))
+  i = i+1
+}
+
+i = 1
+for(li in snp_imp_links) {
+  download.file(url = li, destfile = paste0("./atlasFiles/snps.imputed.chr",i,".csv.gz"))
+  i = i+1
+}
+
+# unzip files
+FILES = list.files("./atlasFiles/", full.names = T)
+for(fi in FILES) {
+  R.utils::gunzip(fi)
+}
