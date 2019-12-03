177ee74c5bb4815469d9ecd91341334b1ba8ad60
Matthew Parker <mrparker909@gmail.com>
2019-12-03 14:49:24 -0800

    Update catTempFiles.R

--- Rscripts/catTempFiles.R
+++ Rscripts/catTempFiles.R
@@ -1,23 +1,23 @@
-cFiles = list.files(pattern="./tmp/atlas\\.tmp")
+cFiles = list.files(path="./tmp", pattern="atlas\\.tmp*", full.names=T)
 
-if(!length(cFiles > 0)) { stop("ERROR: atlas.tmp* file(s) not found") }
+if(!length(cFiles > 0)) { stop("ERROR: atlas.tmp* files not found") }
 
-df = read.table(cFiles[1], sep="\t", header=T, quote="", row.names=F, stringsAsFactors=F)
+df = read.table(cFiles[1], sep="\t", header=T, quote="", stringsAsFactors=F)
 for(fi in cFiles[-1]) {
-  df1 = read.table(cFiles[fi], sep="\t", header=T, quote="", row.names=F, stringsAsFactors=F)
+  df1 = read.table(fi, sep="\t", header=T, quote="", stringsAsFactors=F)
   df = rbind(df,df1)
 }
 
 write.table(df, "tmp/atlas_cat.tmp", quote=F, sep="\t",row.names=F)
 
 ##
-cFiles = list.files(pattern="./tmp/atlas\\.snp\\.tmp*")
+cFiles = list.files(path="./tmp", pattern="atlas\\.snp\\.tmp*", full.names=T)
 
-if(!length(cFiles > 0)) { stop("ERROR: atlas.snp.tmp* file(s) not found") }
+if(!length(cFiles > 0)) { stop("ERROR: atlas.snp.tmp* files not found") }
 
-df = read.table(cFiles[1], sep="\t", header=T, quote="", row.names=F, stringsAsFactors=F)
+df = read.table(cFiles[1], sep="\t", header=T, quote="", stringsAsFactors=F)
 for(fi in cFiles[-1]) {
-  df1 = read.table(cFiles[fi], sep="\t", header=T, quote="", row.names=F, stringsAsFactors=F)
+  df1 = read.table(fi, sep="\t", header=T, quote="", stringsAsFactors=F)
   df = rbind(df,df1)
 }
 
